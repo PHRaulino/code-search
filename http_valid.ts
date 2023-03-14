d0b3edf9330ecc250124d03ec3ce0db461ebdb09
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-03-14 16:04:33 -0300

    Update http_valid.ts

--- http_valid.ts
+++ http_valid.ts
@@ -1,37 +1,7 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { loadRemoteEntry } from './remote-entry';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-const availableUrls = ['http://exemplo.com', 'http://outraurl.com', 'http://maisumaurl.com'];
-
-const loadRemoteEntries = availableUrls.map(url => loadRemoteEntry(url));
-
-Promise.allSettled(loadRemoteEntries).then(results => {
-  const rejectedUrls = [];
-
-  results.forEach((result, index) => {
-    if (result.status === 'rejected') {
-      const url = availableUrls[index];
-      const reason = result.reason;
-      rejectedUrls.push({ url, reason });
-    }
-  });
-
-  if (rejectedUrls.length > 0) {
-    console.error('Erro ao carregar as seguintes URLs:');
-    rejectedUrls.forEach(rejectedUrl => {
-      console.error(`${rejectedUrl.url}: ${rejectedUrl.reason}`);
+.catch((reasons: any[]) => {
+    const failedUrls = reasons.map((reason, index) => {
+      return { url: urls[index], reason };
     });
-  } else {
-    platformBrowserDynamic().bootstrapModule(AppModule)
-      .catch(err => console.error(err));
-  }
-}).catch(error => {
-  console.error(error);
-});
+    console.error('Erro ao carregar as seguintes URLs:');
+    console.error(failedUrls);
+  });
