4139e4f8d1f4a3bd3a1da01bbb184eb1f41bd9b9
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-03-14 15:50:31 -0300

    Update http_valid.ts

--- http_valid.ts
+++ http_valid.ts
@@ -2,21 +2,36 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { loadRemoteEntry } from './remote-entry';
 
 if (environment.production) {
   enableProdMode();
 }
 
-// realiza uma solicitação HTTP para verificar se a URL está disponível
-const http = new HttpClient(null);
-http.get('http://exemplo.com').subscribe(
-  response => {
-    console.log('A URL está acessível');
+const availableUrls = ['http://exemplo.com', 'http://outraurl.com', 'http://maisumaurl.com'];
+
+const loadRemoteEntries = availableUrls.map(url => loadRemoteEntry(url));
+
+Promise.allSettled(loadRemoteEntries).then(results => {
+  const rejectedUrls = [];
+
+  results.forEach((result, index) => {
+    if (result.status === 'rejected') {
+      const url = availableUrls[index];
+      const reason = result.reason;
+      rejectedUrls.push({ url, reason });
+    }
+  });
+
+  if (rejectedUrls.length > 0) {
+    console.error('Erro ao carregar as seguintes URLs:');
+    rejectedUrls.forEach(rejectedUrl => {
+      console.error(`${rejectedUrl.url}: ${rejectedUrl.reason}`);
+    });
+  } else {
     platformBrowserDynamic().bootstrapModule(AppModule)
       .catch(err => console.error(err));
-  },
-  error => {
-    console.log('A URL não está acessível');
   }
-);
+}).catch(error => {
+  console.error(error);
+});
