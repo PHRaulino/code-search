b2fe19f5b68f93fc923c5148503d1faa0da48abf
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2023-07-27 10:04:37 -0300

    Update Code_share.py

--- Code_share.py
+++ Code_share.py
@@ -1,12 +1,24 @@
 import qrcode
 import argparse
+import os
 
 # Crie o analisador de argumentos
 parser = argparse.ArgumentParser(description='Gere um QR Code a partir de um texto fornecido.')
-parser.add_argument('data', type=str, help='O texto a ser convertido em QR Code')
+parser.add_argument('data', type=str, nargs='?', default='', help='O texto a ser convertido em QR Code')
 
 # Analise os argumentos
 args = parser.parse_args()
+
+data = args.data
+
+# Verifique se algum argumento foi fornecido
+if data == '':
+    # Tente ler de um arquivo de texto
+    try:
+        with open('input.txt', 'r') as file:
+            data = file.read().replace('\n', '')
+    except FileNotFoundError:
+        print("Por favor, forneça um argumento ou coloque os dados em um arquivo chamado 'input.txt'.")
 
 # Crie uma instância qr
 qr = qrcode.QRCode(
@@ -17,7 +29,7 @@ qr = qrcode.QRCode(
 )
 
 # Adicione os dados ao qr
-qr.add_data(args.data)
+qr.add_data(data)
 qr.make(fit=True)
 
 # Imprima o QR Code no terminal
